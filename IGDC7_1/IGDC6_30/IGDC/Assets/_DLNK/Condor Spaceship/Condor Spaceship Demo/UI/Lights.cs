20ea7eef4a69ede5543225816e29437b15af10df
AnHyoungBin <nks00404@gmail.com>
2017-07-19 16:08:23 +0900

    IGDC

---
+++ IGDC7_1/IGDC6_30/IGDC/Assets/_DLNK/Condor Spaceship/Condor Spaceship Demo/UI/Lights.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lights : MonoBehaviour {
+
+	public GameObject LightGroup;
+
+	// Use this for initialization
+	void OnMouseDown () {
+		if (LightGroup.activeInHierarchy){
+			LightGroup.SetActive(false);
+		}
+		else
+		{LightGroup.SetActive(true);}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
